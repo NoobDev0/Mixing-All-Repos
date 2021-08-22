1780c1bba657eff06bf3a3a7a4ad33b4e4541842
Noob Dev <88297858+NoobDev0@users.noreply.github.com>
2021-08-22 13:58:56 +0530

    Add files via upload

---
+++ Adv-Auto-Filter-Bot-V2/bot/translation.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+class Translation(object):
+    
+    START_TEXT = """<u><b>Hey {}!!</b></u>
+<i>What's Up '<b>Film Buff!</b>'🎬</i>\n\n ᴀᴍ ᴊᴜsᴛ ᴀ ᴀᴅᴠᴀɴᴄᴇ ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ.... ᴡɪᴛʜ ᴍᴀɴʏ ᴘᴏᴡᴇʀs!🔥
+ 
+<b>ɪ ᴀᴍ ᴄʀᴇᴀᴛᴇᴅ ᴛᴏ sᴇʀᴠᴇ <u>ᗩՏK ᗩ TO ᘔ ᗰOᐯIᗴՏ</u></b>🎊 
+🎭Click The <b>Help</b> To See And Unlock My Full Potential! \n<i>Join Our Official Channel</i>
+@CinemaHaunter 
+</i>"""    
+    
+    HELP_TEXT = """⚡<b>Given Below are Some Features With Which You Can Mess Me</b>⚡
+
+@CinemaHaunter
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>   
+
+<b>➥ Creator</b> : <b><i><a href="https://t.me/JonasKhanwaldTG">Jonas</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Source Code</b> : <i><a href="https://telegra.ph/file/21d3243265c04d0f27f1e.jpg">Click Me</a></i>
+"""
+    FILESHARE_TEXT = """𝗙𝗜𝗟𝗘 𝗦𝗛𝗔𝗥𝗘 𝗛𝗘𝗟𝗣 
+
+  File share stores files in channel as a database and gets you the files in your pm through links with multiple files support Here are the available commands   
+  
+
+▪️/batch - uses to send multiple files with one link 
+
+▪️/genlink - send a file(or any) to bots pm from database channel  and reply to that file with /genlink for creating a permanent link for that file
+
+@CinemaHaunter 
+"""
+    VCPLAYER_TEXT = """VOICE CHAT HELP
+
+Vc Player Plays Music In Groups And Channels! 🎧
+Check Out this Article For More Info About Commands!
+
+<a href="https://telegra.ph/The-Help-07-30">Daisy X Music Commands </a>
+
+@CinemaHaunter 
+"""
+    FILTER_TEXT = """𝗙𝗜𝗟𝗧𝗘𝗥 𝗛𝗘𝗟𝗣 
+
+ Here Is the available commands in filter
+
+
+▪️/filter - for saving a filter works like a normal for filter so.. no tutorial 
+
+▪️/rem - for deleting a filter 
+
+▪️/remall - for deleting all filters (think twice before doing it) 
+
+▪️/viewfilters - list all saved filters 
+
+▪️/set - for connecting into pm 
+
+▪️/sets - view/manage connected groups 
+
+▪️/unset - disconnect a chat from connection 
+
+▪️/info - gives information about given user 
+
+▪️/id - get user id
+
+@CinemaHaunter 
+"""
+    AUTOFILTER_TEXT = """There's Nothig Special in this
+"""
+    REQ_IN_PM = """<b>Hey Chum Thanks For Using Me!</b>
+<i>Don't Request Your Movie Here Request In @Cinema_Haunter!
+
+Our Official Channel - @CinemaHaunter
+"""
+    SOURCE_TEXT = """Given Here Are the Repositories That I Used In Making These Powers
+
+𝗔𝗨𝗧𝗢 𝗙𝗜𝗟𝗧𝗘𝗥
+Repo: <a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot-V2/">REPO</a>
+
+𝗨𝗡𝗟𝗜𝗠𝗜𝗧𝗘𝗗 𝗙𝗜𝗟𝗧𝗘𝗥
+Repo: <a href="https://github.com/TroJanzHEX/Unlimited-Filter-Bot/">REPO</a>
+
+𝗙𝗜𝗟𝗘 𝗦𝗧𝗢𝗥𝗘
+Repo: <a href="https://github.com/CodeXBotz/File-Sharing-Bot/">REPO</a>
+
+𝗩𝗖 𝗣𝗟𝗔𝗬𝗘𝗥
+Repo: <a href="https://github.com/TeamDaisyX/DaisyXMusic/">REPO</a>
+"""
