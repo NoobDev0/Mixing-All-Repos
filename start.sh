2a864adf1de830e0b78992273df833ec9f6d46ab
Noob Dev <88297858+NoobDev0@users.noreply.github.com>
2021-08-23 18:34:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 echo "Cloning Repo...."
-git clone https://github.com/NoobDev0/Mixing-All-Repos.git /MusicPlayer
+git clone https://github.com/NoobDev0/Mixing-All-Repos.git 
 cd /Mixing-All-Repos
 cd /Adv-Auto-Filter-Bot-V2
 echo "Starting Filter Bot...."
 python3 -m bot
-cd
+cd ..
 cd /Unlimited-Filter-Bot 
 echo "Starting Unlimited Filter Bot"
 python3 botone.py
-cd
+cd ..
 cd /File-Sharing-Bot 
 echo "Starting File Sharing Bot"
 python3 mainthree.py
-cd
+cd ..
 cd /MusicPlayer 
 echo "Starting Music Player"
 python3 main.py
